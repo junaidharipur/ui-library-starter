992788df23d86aff7ff7a2489a2fd03172195332
junaidharipur <eng.junaid.javed@gmail.com>
2023-11-01 13:40:19 +0500

    feat: added component exports

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ export * from "./components/Button/Button";
 export * from "./components/ButtonGroup/ButtonGroup";
 export * from "./components/Alert/Alert";
 export * from "./components/Typography/Typography";
+export * from "./components/Breadcrumbs/Breadcrumbs";
+export * from "./components/Checkbox/Checkbox";
+export * from "./components/ConfirmationModal/ConfirmationModal";
+export * from "./components/Dropdown/dropdown";
+export * from "./components/FormInputs/FormInput";
