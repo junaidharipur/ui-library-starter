35770ed3bf74d2a60296e0dbf4adf953a49a867d
junaidharipur <eng.junaid.javed@gmail.com>
2023-11-03 16:53:07 +0500

    feat: extend progress bar

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -2,12 +2,14 @@ import * as React from "react";
 
 import cx from "classnames";
 
-export function ProgressBar({ percentage = 50 }: ProgressBarProps) {
+export function ProgressBar({ percentage = 50, color }: ProgressBarProps) {
     return (
         <div className={cx("bg-grey-10 w-full h-[7px] rounded-full overflow-hidden inline-block")}>
             <span
-                style={{ width: `${percentage}%` }}
-                className={cx("inline-block h-[7px] absolute bg-teal-light-4 rounded-full")}
+                style={{ width: `${percentage}%`, ...(color ? { color: color } : {}) }}
+                className={cx("inline-block h-[7px] absolute rounded-full", {
+                    ["bg-teal-light-4"]: !color,
+                })}
             />
         </div>
     );
@@ -15,4 +17,5 @@ export function ProgressBar({ percentage = 50 }: ProgressBarProps) {
 
 interface ProgressBarProps {
     percentage?: number;
+    color?: string;
 }
