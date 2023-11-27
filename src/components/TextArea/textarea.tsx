5acf94b7393bc812e4abef3e5f416a514772406c
akashahmad <engr.akashahmad@gmail.com>
2023-11-27 19:24:19 +0500

    feat: implement textarea component

--- src/components/TextArea/textarea.tsx
+++ src/components/TextArea/textarea.tsx
@@ -1,58 +1,51 @@
-import React, { ChangeEvent, ReactElement } from "react";
+import React, { ChangeEventHandler, ReactElement } from "react";
 import cx from "classnames";
 
-export function FormInput({
+export function TextArea({
     variant = "Default",
     size = "Small",
     label,
     helperText,
     palceholder,
     value,
-    type = "text",
-    icon: Icon,
     name,
     required,
     readOnly,
     className,
+    rows = 3,
+    cols,
     onChange,
 }: InputProps): ReactElement {
     return (
         <div className="flex flex-col gap-[5px]">
             <span
                 className={cx(
-                    " flex gap-1",
+                    "text-dark flex gap-1",
                     {
                         "text-lg font-medium ": size === "Large",
                         "text-base font-medium leading-6": size === "Medium",
                         "text-sm font-medium leading-6 ": size === "Small",
-                        "text-gray-500 ": readOnly,
-                        "text-dark": !readOnly,
+                        "text-gray-500": readOnly,
                     },
                     className,
                 )}
             >
                 <p>{label}</p> {required && <span>*</span>}
             </span>
-            <div className="relative">
-                {Icon && (
-                    <div className="absolute inset-y-0 right-0 flex items-center pr-3.5 pointer-events-none cursor-pointer">
-                        <Icon />
-                    </div>
-                )}
-                <input
+            <div className="">
+                <textarea
                     name={name}
-                    type={type}
                     value={value}
                     onChange={onChange}
                     className={cx(
-                        " text-primary-text border placeholder-input-placeholder text-base font-normal rounded-md  block w-full",
+                        "text-primary-text py-[3px] pl-5 pr-4 border placeholder-input-placeholder text-base font-normal rounded-md focus:ring-primary focus:outline-primary focus:border-primary block w-full",
                         {
                             "border-stroke": variant === "Default",
                             "border-green": variant === "Success",
                             "border-red": variant === "Error",
-                            "py-2 pl-5 pr-4 ": size === "Large",
-                            "py-[5px] pl-5 pr-4": size === "Medium",
-                            "py-[3px] pl-5 pr-4 ": size === "Small",
+
+                            "bg-grey-3": readOnly,
+                            "text-gray-500": readOnly,
                             "bg-grey-3 focus:ring-0 focus:outline-0 focus:border-none": readOnly,
                             "focus:ring-primary focus:outline-primary focus:border-primary": !readOnly,
                         },
@@ -60,6 +53,8 @@ export function FormInput({
                     placeholder={palceholder}
                     readOnly={readOnly}
                     required={required}
+                    rows={rows}
+                    cols={cols}
                 />
             </div>
             <p
@@ -82,12 +77,12 @@ interface InputProps {
     label?: string;
     helperText?: string;
     value?: string | ReadonlyArray<string> | number | undefined;
-    onChange?: (_e: ChangeEvent<HTMLInputElement>) => void;
+    onChange?: ChangeEventHandler | undefined;
     palceholder?: string;
-    type?: "number" | "text" | "password" | "email" | "date" | "range";
-    icon?: React.ComponentType;
     name?: string;
     required?: boolean;
     readOnly?: boolean;
     className?: string;
+    rows?: number;
+    cols?: number;
 }
